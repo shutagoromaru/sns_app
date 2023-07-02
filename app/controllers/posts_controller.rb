cc75fbdd6591a82603adf9460fffb3f11df2a364
shuta goromaru <560consultant@gmail.com>
2023-07-02 13:45:52 +0000

    Initial commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,48 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+ # このActionを編集してください
+  def index
+    @title = params[:title]
+    if @title.present?
+      @posts = Post.where('title LIKE ?', "%#{@title}%")
+    else
+      @posts = Post.all
+    end
+    debugger
+
+    render :index
+  end
+  # ここまで
+
+ # ここから
   def new
+    @post = Post.new
     render :new
-  end 
+  end
+
+  def create
+    @post = Post.new(post_params)
+  
+    if params[:post][:image]
+      @post.image.attach(params[:post][:image])
+    end
+
+    if @post.save
+      redirect_to index_post_path, notice: '登録しました'
+      
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+  # ここまで
 end
+　# 課題２－６
+def edit
+  render :edit
+end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'posts/new', to: 'posts#new', as: 'new_posts'
-  get 'topics/new', to: 'topics#new',as: 'new_topics'
-  get 'topics/:id/edit' , to: 'topics#edit', as: 'edit_topics'
+
+  # PostsController
+  get '/', to: 'posts#index', as: 'index_post'
+  get 'posts/new', to: 'posts#new', as: 'new_post'
+  # この行を追加
+  post 'posts/new', to: 'posts#create', as: 'create_post'
+
+  # TopicsController
+  get 'topics/new', to: 'topics#new', as: 'new_topic'
+  get 'topics/edit/:id', to: 'topics#edit', as: 'edit_topic'
+
 end
